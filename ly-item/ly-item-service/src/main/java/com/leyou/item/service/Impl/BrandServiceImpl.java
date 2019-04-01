eeaf5219420ef7328ea5c90bd87d8c660487d550
Taurusbing <760494282@qq.com>
2019-04-01 11:20:08 +0800

    使用函数式编程优化代码

--- ly-item/ly-item-service/src/main/java/com/leyou/item/service/Impl/BrandServiceImpl.java
+++ ly-item/ly-item-service/src/main/java/com/leyou/item/service/Impl/BrandServiceImpl.java
@@ -1,5 +1,6 @@
 package com.leyou.item.service.Impl;
 
+import com.github.pagehelper.ISelect;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.leyou.item.dao.BrandMapper;
@@ -28,9 +29,23 @@ public class BrandServiceImpl implements BrandService {
     @Override
     public PageResult<Brand> queryAllByParams(int page, int rows, String search, boolean descending, String sortBy) {
         //开始分页
-        PageHelper.startPage(page, rows);
+//        PageHelper.startPage(page, rows);
+//        Page<Brand> pageInfo = (Page<Brand>) brandMapper.queryAllByParams(search, descending, sortBy);
 
-        Page<Brand> pageInfo = (Page<Brand>) brandMapper.queryAllByParams(search, descending, sortBy);
+        //匿名内部类方式
+//        Page<Brand> pageInfo1 = PageHelper.startPage(page, rows)
+//                .doSelectPage(new ISelect() {
+//                    @Override
+//                    public void doSelect() {
+//                        brandMapper.queryAllByParams(search, descending, sortBy);
+//                    }
+//                });
+
+        //使用函数式编程方式
+        Page<Brand> pageInfo = PageHelper.startPage(page, rows)
+                .doSelectPage(() -> brandMapper.queryAllByParams(search, descending, sortBy));
+
         return new PageResult<Brand>(pageInfo.getTotal(),pageInfo);
+
     }
 }
